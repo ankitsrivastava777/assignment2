2e7dfeb01069ad64d8482377d615790df6612ac1
ankitsrivastava777 <ankit@excellencetechnologies.info>
2021-06-30 13:55:15 +0530

    added distance between two api

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/airport', require('./routes/airport'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3200, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
--- routes/airport.js
+++ routes/airport.js
@@ -6,6 +6,25 @@ const fs = require("fs");
 var data = fs.readFileSync("./airport.json", "utf-8");
 var airportData = JSON.parse(data);
 
+function calcCrow(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+// Converts numeric degrees to radians
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
+}
+
 app.get("/list", function (req, res) {
   try {
     if (data) {
@@ -42,35 +61,14 @@ app.post("/nearest/:locationId", function (req, res) {
     }
     var locationId = req.params.locationId;
 
-    var airportData = airportData.find((x) => x.LocationID === locationId);
+    var cordinates = airportData.find((x) => x.LocationID === locationId);
 
-    function calcCrow(lat1, lon1, lat2, lon2) {
-      var R = 6371; // km
-      var dLat = toRad(lat2 - lat1);
-      var dLon = toRad(lon2 - lon1);
-      var lat1 = toRad(lat1);
-      var lat2 = toRad(lat2);
-
-      var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.sin(dLon / 2) *
-          Math.sin(dLon / 2) *
-          Math.cos(lat1) *
-          Math.cos(lat2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      var d = R * c;
-      return d;
-    }
-    // Converts numeric degrees to radians
-    function toRad(Value) {
-      return (Value * Math.PI) / 180;
-    }
     var distance = [];
     for (i = 0; i < airData.length; i++) {
       distance.push({
         distanceForm_A: calcCrow(
-          airportData.Lat,
-          airportData.Lon,
+          cordinates.Lat,
+          cordinates.Lon,
           airportData[i]["Lat"],
           airportData[i]["Lon"]
         ),
@@ -92,6 +90,41 @@ app.post("/nearest/:locationId", function (req, res) {
       error: 0,
       message: "nearest airports to the customer",
       data: nearest_distance,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: 1,
+      message: err.message,
+      data: null,
+    });
+  }
+});
+
+app.post("/distance/:locationid1/:locationid2", function (req, res) {
+  try {
+    var locationId1 = req.params.locationid1;
+    var locationId2 = req.params.locationid2;
+
+    var cordinates1 = airportData.find((x) => x.LocationID === locationId1);
+    var cordinates2 = airportData.find((x) => x.LocationID === locationId2);
+
+    console.log("hwllo" + cordinates2.Lat);
+    var distanceBetweenTwo = calcCrow(
+      cordinates1.Lat,
+      cordinates1.Lon,
+      cordinates2.Lat,
+      cordinates2.Lon
+    );
+    res.status(200).json({
+      error: 0,
+      message: "distance between two airports",
+      data:
+        "distance between " +
+        cordinates1.LocationID +
+        " and " +
+        cordinates2.LocationID +
+        " is " +
+        distanceBetweenTwo,
     });
   } catch (err) {
     res.status(500).json({
